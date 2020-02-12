007e6c564750bc23fc9a2d8a59903776d3fd77f3
GersonCaballero <gersoncaballero306@gmail.com>
2020-02-12 15:53:54 -0600

    Create return inventary requisition and testing

--- leaf_develop/account_status/doctype/inventory_requisition/test_inventory_requisition.py
+++ leaf_develop/account_status/doctype/inventory_requisition/test_inventory_requisition.py
@@ -3,24 +3,63 @@
 # See license.txt
 from __future__ import unicode_literals
 
-# import frappe
+import frappe
 import unittest
+
+def create_requisitions():
+    if frappe.flags.test_events_created:
+        return
+
+    frappe.set_user("Administrator")
+    doc = frappe.get_doc({
+        "doctype": "Inventory Requisition",
+		"name": "Test1",
+        "patient_statement": 'Prueba',
+		"date_create":'10-02-2020 15:34:04',
+		"products": [{
+			"item": 'CLI-SER-0003',
+			"product_name": 'Colchon de agua',
+			"quantity": 2
+		}],
+		"state": 'Open'
+    }).insert()
+
+    doc = frappe.get_doc({
+        "doctype": "Inventory Requisition",
+		"name": "Test2",
+        "patient_statement": 'Prueba',
+		"date_create":'10-02-2020 16:34:04',
+		"products": [{
+			"item": 'CLI-SER-0003',
+			"product_name": 'Colchon de agua',
+			"quantity": 2
+		}],
+		"state": 'Open'
+    }).insert()
+
+    doc = frappe.get_doc({
+        "doctype": "Inventory Requisition",
+		"name": "Test3",
+        "patient_statement": 'Prueba',
+		"date_create":'10-02-2020 17:34:04',
+		"products": [{
+			"item": 'CLI-SER-0003',
+			"product_name": 'Colchon de agua',
+			"quantity": 2
+		}],
+		"state": 'Open'
+    }).insert()
+
+    frappe.flags.test_events_created = True
 
 class TestInventoryRequisition(unittest.TestCase):
 	def setUp(self):
-		data(setup=True)
+		create_requisitions()
+	
+	def tearDown(self):
+		frappe.set_user("Administrator")
 
-def data(setup=False, test_tax=False):
-	data = [
-		{
-			"patient_statement": 'Prueba',
-			"date_create":'10-02-2020 15:34:04',
-			"products": [{
-				"item": 'CLI-SER-0003',
-				"product_name": 'Colchon de agua',
-				"quantity": 2
-			}],
-			"state": 'Open'
-		}
-	]
-	return data
+	def get_register(self):
+		frappe.set_user("test1@example.com")
+		doc = frappe.get_doc("Inventory Requisition", frappe.db.get_value("Inventory Requisition",{"name":"Test1"}))
+		self.assertTrue(doc.name)
