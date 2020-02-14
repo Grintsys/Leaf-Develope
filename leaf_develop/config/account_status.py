c3b3d302ac59b1215e3a2eb143a46cf614891f86
GersonCaballero <51335862+GersonCaballero@users.noreply.github.com>
2020-02-14 11:06:36 -0600

    Update account_status.py

--- leaf_develop/config/account_status.py
+++ leaf_develop/config/account_status.py
@@ -2,40 +2,4 @@ from __future__ import unicode_literals
 from frappe import _
 
 def get_data():
-    return [
-        {
-			"label": _("Account Status"),
-			"items": [
-				{
-					"type": "doctype",
-					"name": "Patient statement",
-					"onboard": 1,
-				},
-				{
-					"type": "doctype",
-					"name": "Medical Honorarium",
-					"onboard": 1,
-				}
-			]
-		},
-		{
-			"label": _("Doctors"),
-			"items": [
-				{
-					"type": "doctype",
-					"name": "Medical",
-					"onboard": 1,
-				},
-				{
-					"type": "doctype",
-					"name": "Medical Category",
-					"onboard": 1,
-				},
-				{
-					"type": "doctype",
-					"name": "Honorarium Payment",
-					"onboard": 1,
-				}
-			]
-		}
-    ]
+    pass
